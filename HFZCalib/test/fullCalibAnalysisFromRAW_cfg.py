92193a1c41f7ef21f972151e4403df8cb493d7c1
bdahmes <>
2012-11-13 10:58:54 +0000

    Included gen filter, additional switches for more robust running on MC/data

--- HFZCalib/test/fullCalibAnalysisFromRAW_cfg.py
+++ HFZCalib/test/fullCalibAnalysisFromRAW_cfg.py
@@ -9,20 +9,45 @@ process = cms.Process("HFCALIB")
 #--- (4) Filtering on ECAL+HF, Z mass            ---#
 #--- (5) HF Calibration analysis                 ---#
 triggerFilter = True
-nvertexFilter = True
+nvertexFilter = False
 doRerecoOnRaw = True
 zFilterEcalHF = True
 calibAnalysis = True
 
+#--- Filter out events with no HF EM cluster above 5 GeV ---#
+doHFclusterFiltering = True
+
+#--- Which data era? ---#
+#--- 532, not isRun2012C: Using data re-reco, Run 2012A/B, CMSSW version = 5_3_2_patch4         ---#
+#--- 532, isRun2012C:     Using prompt reco, Run 2012C, Version 1, CMSSW version = 5_3_2_patch4 ---#
+#--- 533, isRun2012C:     Using prompt reco, Run 2012C, Version 2, CMSSW version >= 5_3_3       ---#
+#--- 533, not isRun2012C: Using prompt reco, Run 2012D, CMSSW version >= 5_3_3                  ---#
+cmsswVersion = 532
+isRun2012C   = True
+
+#--- Cut levels: ---#
+# 0: Testing only
+# 1: Loose 2D,  e9/e25 = 0.80
+# 2: Loose 2D,  e9/e25 = 0.92 <--- Recommended cut level
+# 3: Medium 2D, e9/e25 = 0.96 
+cutLevel = 2
+
 #--- Testing flag: Do not enable unless needed! ---#
 testing = False
 
+#-----------------------------------#
 #--- Flag for running on data/MC ---#
+#-----------------------------------#
 isData = True
+
 #--- If running on MC, and nvertexFilter is True, then set the ---#
 #--- inclusive range of vertices considered: [vtxMin,vtxMax]   ---#
 vtxMin = 0 
 vtxMax = 3
+#--- If nvertexFilter is False, simply count vertices ---#
+if not nvertexFilter: 
+    vtxMin = 0
+    vtxMax = -1
 
 #--- Flag for keeping the skim results ---#
 keepSkimEvents = False
@@ -38,7 +63,7 @@ if isData:
         vtxMin = 0 
         vtxMax = -1 
 else:
-    if keepSkimEvents:
+    if keepSkimEvents and not testing:
         print "WARNING: Running on MC but saving skimmed events.  FIX: Disabling keepSkimEvents."
         keepSkimEvents = False
     if triggerFilter:
@@ -58,7 +83,25 @@ process.load('FWCore/MessageService/MessageLogger_cfi')
 process.MessageLogger.cerr.FwkReport.reportEvery = 1000
 process.load('Configuration.EventContent.EventContent_cff')
 process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+
 process.load("RecoEgamma.EgammaHFProducers.hfEMClusteringSequence_cff")
+if cutLevel == 0:
+    process.hfRecoEcalCandidate.e9e25Cut = 0.0 
+    process.hfRecoEcalCandidate.intercept2DCut   = 0.0 
+    process.hfRecoEcalCandidate.intercept2DSlope = 0.0 
+if cutLevel == 1:
+    process.hfRecoEcalCandidate.e9e25Cut = 0.80 
+    process.hfRecoEcalCandidate.intercept2DCut   = 0.815
+    process.hfRecoEcalCandidate.intercept2DSlope = 0.475
+if cutLevel == 2:
+    process.hfRecoEcalCandidate.e9e25Cut = 0.92
+    process.hfRecoEcalCandidate.intercept2DCut   = 0.815
+    process.hfRecoEcalCandidate.intercept2DSlope = 0.475
+if cutLevel == 3:
+    process.hfRecoEcalCandidate.e9e25Cut = 0.96
+    process.hfRecoEcalCandidate.intercept2DCut   = 0.875
+    process.hfRecoEcalCandidate.intercept2DSlope = 0.275
+
 if isData and doRerecoOnRaw:
     process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff')
     process.load('Configuration.StandardSequences.RawToDigi_Data_cff')
@@ -75,7 +118,18 @@ if isData and doRerecoOnRaw:
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 if isData:
     if doRerecoOnRaw:
-        process.GlobalTag.globaltag = cms.string('FT_R_53_V6::All')
+        if cmsswVersion == 533:
+            if not isRun2012C: # Run 2012D
+                process.GlobalTag.globaltag = cms.string('GR_P_V42_AN2::All')
+            else: # Run 2012C, v2
+                process.GlobalTag.globaltag = cms.string('GR_P_V41_AN2::All')
+        else:
+            if cmsswVersion != 532:
+                print "WARNING: Unrecognized CMSSW version.  Assuming 532 and continuing"
+            if isRun2012C: # Run 2012C, v1
+                process.GlobalTag.globaltag = cms.string('GR_P_V40_AN2::All')
+            else:
+                process.GlobalTag.globaltag = cms.string('FT_R_53_V6::All')
     else:
         process.GlobalTag.globaltag = cms.string('GR_P_V41_AN2::All')
 else:
@@ -148,6 +202,36 @@ if hfPhiSymCorrections:
 if doRerecoOnRaw:
     process.reconstructionFromRawSequence = cms.Sequence(process.RawToDigi * process.L1Reco * process.reconstruction)
 
+## Need to redo the HF clustering on Monte Carlo: requires RECO, not AOD
+if not isData:
+    process.redoHFclustering = cms.Sequence(process.hfEMClusteringSequence)
+
+if doHFclusterFiltering:
+    # turn HF superclusters into candidates
+    process.hfCandidateSuperClusters = cms.EDProducer("ConcreteEcalCandidateProducer",
+        src = cms.InputTag("hfEMClusters"),
+        particleType = cms.string("gamma")
+    )
+    process.hfSC = cms.EDFilter("CandViewSelector",
+        src = cms.InputTag("hfCandidateSuperClusters"),
+        cut = cms.string('abs( eta ) > 2.9 & abs( eta ) < 5.1')
+    )
+    # select HF sucperclusters above a certain Et
+    process.hfCut = cms.EDFilter("EtaPtMinCandViewSelector",
+        src    = cms.InputTag("hfSC"),
+        etaMin = cms.double(-5.1),
+        etaMax = cms.double(5.1),
+        ptMin  = cms.double(5.0)
+    )
+    # this is the FILTER which requires at least N=1 HF SC above a certain Et 
+    process.hfSel = cms.EDFilter("CandViewCountFilter",
+        src = cms.InputTag("hfCut"),
+        minNumber = cms.uint32(1)
+    )
+    process.hfFilterSequence = cms.Sequence( process.hfCandidateSuperClusters + process.hfSC + process.hfCut + process.hfSel )
+    if doRerecoOnRaw:
+        process.rawHFfilterSequence = cms.Sequence(process.RawToDigi + process.hfreco + process.hfEMClusters + process.hfFilterSequence )
+
 ###--- (4) Require Z->ee, ECAL+HF ---###
 if zFilterEcalHF:
     process.load('RecoJets.JetProducers.kt4PFJets_cfi') # For isolation calculation
@@ -160,6 +244,7 @@ if zFilterEcalHF:
     import ElectroWeakAnalysis.WENu.simpleCutBasedElectronIDSpring10_cfi
     process.load("RecoLocalCalo.EcalRecAlgos.EcalSeverityLevelESProducer_cfi")
     process.load("ElectroWeakAnalysis.WENu.simpleEleIdSequence_cff")
+    process.simpleCutBasedElectronID.rhoCorrection = cms.InputTag("kt6PFJetsForZeeCalib","rho")
     process.getElectronIDs = cms.Sequence(process.kt6PFJetsForZeeCalib * process.simpleEleIdSequence)
 
     import HCALCalib.HFZCalib.hfzeefilter_cfi
@@ -168,16 +253,10 @@ if zFilterEcalHF:
     process.hfzeeForCalib.ecalID = cms.string('simpleEleId80cIso')
     process.hfzeeForCalib.DoLog  = cms.bool(True)
     # For testing ONLY!!!
-    if testing:
-        process.hfzeeForCalib.idThreshold = cms.int32(1)
+    # if testing:
+    #     process.hfzeeForCalib.idThreshold = cms.int32(1)
 
-    ## Medium cut values
-    process.load("RecoEgamma.EgammaHFProducers.hfRecoEcalCandidate_cfi")
-    process.hfRecoEcalCandidate.intercept2DCut   = 0.875
-    process.hfRecoEcalCandidate.intercept2DSlope = 0.275
-    process.hfRecoEcalCandidate.e9e25Cut         = 0.96
-
-    process.calibInit = cms.Sequence( process.getElectronIDs * process.hfRecoEcalCandidate ) 
+    process.calibInit = cms.Sequence( process.getElectronIDs ) 
     process.zCalibFilterSequence = cms.Sequence( process.calibInit * process.hfzeeForCalib ) 
 
 ###--- (5) HF Calibration Analysis ---###
@@ -194,6 +273,16 @@ if calibAnalysis:
         fileName = cms.string('HFZCalib_from_data.root')
     )
 
+###--- Debugging ---###
+if testing:
+    process.genFilter = cms.EDFilter( 'ZeeGenFilter', 
+        requireZeeHF  = cms.bool( True ),
+        electronMinET = cms.double( 20.0 ),
+        zMassWindow   = cms.vdouble(60,120),
+        doLog         = cms.bool( True )
+    )
+    process.calibDebuggingSequence = cms.Sequence( process.genFilter )
+
 ###--- Assemble everything ---###
 process.boolTrue = cms.EDFilter( 'HLTBool',
     result = cms.bool( True )
@@ -202,16 +291,24 @@ process.calibPreSequence = cms.Sequence(process.boolTrue)
 
 if triggerFilter:
     process.calibPreSequence += process.triggerFilterSequence
-if nvertexFilter and not isData:
-    process.calibPreSequence += process.vertexFilterSequence
+if doHFclusterFiltering:
+    if doRerecoOnRaw:
+        process.calibPreSequence += process.rawHFfilterSequence 
+    else:
+        process.calibPreSequence += process.hfFilterSequence
 if doRerecoOnRaw:
     process.calibPreSequence += process.reconstructionFromRawSequence
-if nvertexFilter and isData:
-    process.calibPreSequence += process.vertexFilterSequence    
+process.calibPreSequence += process.vertexFilterSequence    
+if not isData:
+    process.calibPreSequence += process.redoHFclustering
 if zFilterEcalHF:
     process.calibPreSequence += process.zCalibFilterSequence
 
-process.p = cms.Path( process.calibPreSequence )
+if not isData and testing:
+    process.p = cms.Path( process.calibDebuggingSequence ) 
+    process.q = cms.Path( process.calibPreSequence )
+else:
+    process.p = cms.Path( process.calibPreSequence )
 
 if zFilterEcalHF and calibAnalysis:
     for nvtx in range(0,50):
